af165cd56a72b24756149f9878effe30390dea2c
LENOVO <LENOVO@DESKTOP-PJ2UI5D>
2024-01-11 17:39:51 +0300

    update 01.11.24

---
+++ BlogWebSite/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,18 @@
+﻿using BlogWebSite.DataAccess.Concrete;
+using BlogWebSite.DataAccess.EntityFramework;
+using BlogWebSite_BussinessLayer.Manager;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebSite.ViewComponents.Comment
+{
+	public class CommentListByBlog : ViewComponent
+	{
+		Context dbContext = new Context();
+		public IViewComponentResult Invoke(int id)
+		{
+			var result = dbContext.Blogs.Include(b => b.Comment).FirstOrDefault(c => c.BlogID == id);
+			return View(result);
+		}
+	}
+}
