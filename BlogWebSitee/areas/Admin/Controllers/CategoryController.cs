1605ca71627c00c5b9d9915c5ef2ca3f666c99a4
KubilayTopaktaş <KubilayTopakta@DESKTOP-PJ2UI5D>
2024-07-02 17:54:17 +0300

    update 02.0724 part 2

---
+++ BlogWebSitee/areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BlogWebSite.DataAccess.EntityFramework;
+using BlogWebSite_BussinessLayer.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebSitee.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var categoryValue = categoryManager.ListAll();
+            ViewData["blogCount"]=categoryManager.ListAll().Where(x=>x.);
+            return View(categoryValue);
+        }
+
+        //Burda kaldık
+
+
+    }
+}
--- BlogWebSitee/areas/Admin/Controllers/EmailController.cs
+++ BlogWebSitee/areas/Admin/Controllers/EmailController.cs
@@ -1,18 +1,42 @@
 ﻿using BlogWebSite.DataAccess.EntityFramework;
 using BlogWebSite_BussinessLayer.Manager;
+using BlogWebSite_Entity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogWebSitee.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class EmailController : Controller
     {
         public MailSystemManager mailSystemManager = new MailSystemManager(new EfMailSystemRepository());
-      
+
+        [HttpGet]
         public IActionResult Index()
         {
             var Mailvalue = mailSystemManager.GetById(1);
 
             return View(Mailvalue);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmail(MailModel mail)
+        {
+            var success = false;
+
+            if(mail is not null)
+            {
+                mailSystemManager.Update(new MailModel
+                {
+                    MailID = mail.MailID,
+                    Port = mail.Port,
+                    ReceiverMail=mail.ReceiverMail,
+                    SenderMail=mail.SenderMail,
+                    SenderMailPassword=mail.SenderMailPassword,
+                    SMTP=mail.SMTP,
+                });
+                success = true;
+            }
+            return Json(new { success = success });
         }
     }
 }
