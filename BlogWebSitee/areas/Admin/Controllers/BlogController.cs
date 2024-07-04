8141e5607a59d669d7752f5d904a00b945805597
KubilayTopaktaş <KubilayTopakta@DESKTOP-PJ2UI5D>
2024-07-04 14:11:30 +0300

    update 04.07.24

---
+++ BlogWebSitee/areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using BlogWebSite.DataAccess.EntityFramework;
+using BlogWebSite_BussinessLayer.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebSitee.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var resultBlogList = blogManager.ListAll();
+
+            return View(resultBlogList);
+        }
+    }
+}
